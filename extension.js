5b1bc12fb0bde091e4cbf01109c61073a6809150
Roberto Huertas <roberto.huertas@outlook.com>
2016-06-01 02:38:02 +0200

    windows paths

--- extension.js
+++ extension.js
@@ -12,12 +12,12 @@ function activate(context) {
     var isWin = /^win/.test(process.platform);
     var appDir = path.dirname(require.main.filename);
 
-    var base = appDir + (isWin ? '\vs\workbench' : '/vs/workbench');
-    var iconFolder = base + (isWin ? '\parts\files\browser\media' : '/parts/files/browser/media');
+    var base = appDir + (isWin ? '\\vs\\workbench' : '/vs/workbench');
+    var iconFolder = base + (isWin ? '\\parts\\files\\browser\\media' : '/parts/files/browser/media');
 
 
-    var cssfile = base + (isWin ? '\workbench.main.css' : '/workbench.main.css');
-    var cssfilebak = base + (isWin ? '\workbench.main.css.bak' : '/workbench.main.css.bak');
+    var cssfile = base + (isWin ? '\\workbench.main.css' : '/workbench.main.css');
+    var cssfilebak = base + (isWin ? '\\workbench.main.css.bak' : '/workbench.main.css.bak');
 
     var cssreplace = '/*! *****************************************************************************';
     var csswith = '.monaco-tree-row .content .sub-content .explorer-item.folder-icon{padding-left:22px;background:url(parts/files/browser/media/icons/Folder_inverse.svg) 1px 4px no-repeat}.monaco-tree-row.expanded .content .sub-content .explorer-item.folder-icon{padding-left:22px;background:url(parts/files/browser/media/icons/Folder_opened.svg) 1px 4px no-repeat;background-size:16px}.explorer-item.text-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/File.svg) 1px 3px no-repeat;background-size:16px}.explorer-item.js-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_js@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.json-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_node@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.jsx-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_react@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.css-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_css@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.scss-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_sass@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.htm-file-icon,.explorer-item.html-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_html@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.bmp-file-icon,.explorer-item.gif-file-icon,.explorer-item.jpeg-file-icon,.explorer-item.jpg-file-icon,.explorer-item.png-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_image@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.svg-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_svg@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.sql-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_sql@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.md-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_markdown@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.yml-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_yaml@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.ts-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_ts@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.dockerfile-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_docker@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.gitattributes-file-icon,.explorer-item.gitignore-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_git@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.otf-file-icon,.explorer-item.ttf-file-icon,.explorer-item.woff-file-icon,.explorer-item.woff2-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_font@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.less-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_less@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.jade-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_jade@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.php-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_php@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.txt-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_txt@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.license-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_license@2x.png) 1px 4px no-repeat;background-size:16px}.explorer-item.bat-file-icon,.explorer-item.cmd-file-icon,.explorer-item.sh-file-icon{padding-left:22px;background:url(parts/files/browser/media/icons/file_type_shell@2x.png) 1px 4px no-repeat;background-size:16px} ';
@@ -32,8 +32,8 @@ function activate(context) {
         });
     }
 
-    var jsfile = base + (isWin ? '\workbench.main.js' : '/workbench.main.js');
-    var jsfilebak = base + (isWin ? '\workbench.main.js.bak' : '/workbench.main.js.bak');
+    var jsfile = base + (isWin ? '\\workbench.main.js' : '/workbench.main.js');
+    var jsfilebak = base + (isWin ? '\\workbench.main.js.bak' : '/workbench.main.js.bak');
 
     var jsreplace = 't.prototype.iconClass=function(e){return e.isDirectory?"folder-icon":"text-file-icon"}';
     var jswith = 't.prototype.iconClass=function(s){if(s.isDirectory)return"folder-icon";var e=s.name.substring(s.name.lastIndexOf(".")+1).toLowerCase();switch(e){case"license":case"cmd":case"bat":case"sh":case"txt":case"php":case"jade":case"ttf":case"otf":case"woff2":case"woff":case"gitattributes":case"gitignore":case"svg":case"gif":case"png":case"sql":case"less":case"dockerfile":case"yml":case"ts":case"jpg":case"js":case"jsx":case"css":case"scss":case"md":case"json":case"html":case"htm":return e+"-file-icon";default:return"text-file-icon"}}';
@@ -75,7 +75,7 @@ function activate(context) {
 
         //add icons to folder
         var zipUrl = 'http://github.com/robertohuertasm/vscode-icons/blob/master/icons.zip?raw=true';
-        k = iconFolder + (isWin ? '\icons.zip' : '/icons.zip');
+        k = iconFolder + (isWin ? '\\icons.zip' : '/icons.zip');
 
         var r = request(zipUrl).pipe(fs.createWriteStream(k));
         r.on('finish', function () {
