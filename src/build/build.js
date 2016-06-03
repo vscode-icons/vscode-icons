e48df38c4730ddb3fb78e86773fb630f9a88e1ca
Roberto Huertas <roberto.huertas@outlook.com>
2016-06-03 19:53:16 +0200

    new icon functionality almost ready

---
+++ src/build/build.js
@@ -0,0 +1,96 @@
+var mustache = require('mustache');
+var _ = require('underscore');
+var extensions = require('./supportedExtensions');
+var fs = require('fs');
+var ncp = require('ncp').ncp;
+
+var cssTemplate =
+  '.monaco-tree-row .content .sub-content .explorer-item.folder-icon {' +
+    'padding-left: 22px;' +
+    'background: url(parts/files/browser/media/icons/Folder_inverse.svg) 1px 4px no-repeat;' +
+  '}'
+  +
+  '.monaco-tree-row.expanded .content .sub-content .explorer-item.folder-icon { ' +
+    'padding-left: 22px;' +
+    'background: url(parts/files/browser/media/icons/Folder_opened.svg) 1px 4px no-repeat;' +
+    'background-size: 16px;' +
+  '}'
+  +
+  '.explorer-item.default-file-icon {' +
+    'padding-left:22px;' +
+    'adding-left:22px;background:url(parts/files/browser/media/icons/File.svg) 1px 3px no-repeat;' +
+    'background-size: 16px;' +
+  '}' +
+  '.explorer-item.file-icon {' +
+    'padding-left:22px;' +
+    'background-size: 16px !important;' +
+  '}' +
+  '{{#supportedExtensions}} ' +
+    '{{#extensions}}' +
+      '.explorer-item.{{.}}-file-icon { ' +
+      'background: url(parts/files/browser/media/icons/file_type_{{icon}}@2x.png) 1px 4px no-repeat;' +
+    '}' +
+   '{{/extensions}}' +
+  '{{/supportedExtensions}}';
+  
+var css = mustache.render(cssTemplate, extensions);
+
+var allExtensions = _.flatten(_.map(extensions.supportedExtensions, function(x){ return x.extensions}));
+
+var specialExtensions = _.uniq(_.filter(extensions.supportedExtensions, function(x) { return x.special;}).map(function(x) {return x.special;}))
+
+var specialSupportedExtensions = _.flatten(_.filter(extensions.supportedExtensions, function(x) { return x.special;}).map(function(x) {return x.extensions;}));
+
+function arrToStr(arr){
+  return JSON.stringify(arr);
+}
+
+var jsView = {
+  allExtensions: arrToStr(allExtensions),
+  specialExtensions: arrToStr(specialExtensions),
+  specialSupportedExtensions : arrToStr(specialSupportedExtensions)
+};
+
+var jsTemplate = 
+'t.prototype.iconClass = function (s) {' +
+  'if (s.isDirectory) return "folder-icon";' +
+  'var fileclass = "-file-icon file-icon";' +
+  'var dot = s.name.lastIndexOf(".");' +
+  'var e = s.name.substring(dot + 1).toLowerCase();' +
+  'var exts = {{{allExtensions}}};' +
+  'var specialExt = {{{specialExtensions}}};' +
+  
+  'if (specialExt.indexOf(e) >= 0) {' +
+    'var special = {{{specialSupportedExtensions}}};' +
+    'var f = s.name.substring(0, dot);' +
+    'if(special.indexOf(f) >=0) return f + fileclass;' +
+  '}' +
+  
+  'if(exts.indexOf(e) >= 0) {' +
+    'return e + fileclass;' +
+    '}else{' +
+    'return "default" + fileclass;'+
+    '}' +
+'}';
+
+
+var js = mustache.render(jsTemplate, jsView);
+
+var finalTemplate = 
+'module.exports = { js: \'{{{js}}}\', css: \'{{{css}}}\' };';
+
+var final = mustache.render(finalTemplate, {js: js, css: css});
+
+//writing the files
+fs.writeFileSync('./src/dev/replacements.js', final);
+console.log('File with replacemnts successfully created!');
+
+//moving to dist
+ncp.limit = 16;
+ncp('./src/dev', './dist', function (err) {
+ if (err) {
+   return console.error(err);
+ }
+ console.log('Build completed!');
+});
+
---
+++ src/dev/extension.js
@@ -0,0 +1,198 @@
+var vscode = require('vscode');
+var fs = require('fs');
+var request = require('request');
+var extract = require('extract-zip');
+var path = require('path');
+var replace = require('replace-in-file');
+var replacements = require('./replacements');
+var events = require('events');
+
+function activate(context) {
+
+    console.log('Congratulations, your extension is now active!');
+
+    var eventEmitter = new events.EventEmitter();
+    var isWin = /^win/.test(process.platform);
+    var appDir = path.dirname(require.main.filename);
+
+    var base = appDir + (isWin ? '\\vs\\workbench' : '/vs/workbench');
+    var iconFolder = base + (isWin ? '\\parts\\files\\browser\\media' : '/parts/files/browser/media');
+
+
+    var cssfile = base + (isWin ? '\\workbench.main.css' : '/workbench.main.css');
+    var cssfilebak = base + (isWin ? '\\workbench.main.css.bak' : '/workbench.main.css.bak');
+
+    var cssreplace = '/*! *****************************************************************************';
+    var csswith = replacements.css + ' \n\n' + cssreplace;
+
+    function replaceCss() {
+        replace({
+            files: cssfile,
+            replace: cssreplace,
+            with: csswith + cssreplace
+        }, function (err, changedFiles) {
+            console.log(err);
+        });
+    }
+
+    var jsfile = base + (isWin ? '\\workbench.main.js' : '/workbench.main.js');
+    var jsfilebak = base + (isWin ? '\\workbench.main.js.bak' : '/workbench.main.js.bak');
+
+    var jsreplace = 't.prototype.iconClass=function(e){return e.isDirectory?"folder-icon":"text-file-icon"}';
+    var jswith = replacements.js;
+
+    function replaceJs() {
+        replace({
+            files: jsfile,
+            replace: jsreplace,
+            with: jswith
+        }, function (err, changedFiles) {
+            console.log(err);
+        });
+    }
+
+    function timeDiff(d1, d2) {
+        var timeDiff = Math.abs(d2.getTime() - d1.getTime());
+        return timeDiff;
+    }
+
+    function hasBeenUpdated(stats1, stats2) {
+        var dbak = new Date(stats1.ctime);
+        var dor = new Date(stats2.ctime);
+        var segs = timeDiff(dbak, dor) / 1000;
+        return segs > 5;
+    }
+
+    function cleanCssInstall() {
+        var c = fs.createReadStream(cssfile).pipe(fs.createWriteStream(cssfilebak));
+        c.on('finish', function () {
+            replaceCss();
+        });
+    }
+
+    function cleanJsInstall() {
+        var j = fs.createReadStream(jsfile).pipe(fs.createWriteStream(jsfilebak));
+        j.on('finish', function () {
+            replaceJs();
+        });
+    }
+
+    function installItem(bakfile, orfile, cleanInstallFunc, replaceFunc) {
+        fs.stat(bakfile, function (errBak, statsBak) {
+            if (errBak) {
+                // clean installation
+                cleanInstallFunc();
+            } else {
+                // check cssfilebak's timestamp and compare it to the cssfile's.
+                fs.stat(orfile, function (errOr, statsOr) {
+                    if (errOr) {
+                        vscode.window.showInformationMessage('Something went wrong: ' + errOr);
+                    } else {
+                        var updated = hasBeenUpdated(statsBak, statsOr);
+                        if (updated) {
+                            // some update has occurred. clean install
+                            cleanInstallFunc();
+                        } else {
+                            replaceFunc();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    function addIcons() {
+        //add icons to folder
+        var zipUrl = 'http://github.com/robertohuertasm/vscode-icons/blob/master/icons.zip?raw=true';
+        k = iconFolder + (isWin ? '\\icons.zip' : '/icons.zip');
+
+        var r = request(zipUrl).pipe(fs.createWriteStream(k));
+        r.on('finish', function () {
+            extract(k, { dir: iconFolder }, function (err) {
+                // extraction is complete. make sure to handle the err 
+                if (err) console.log(err);
+                vscode.window.showInformationMessage('Icons enabled. Restart the IDE.');
+                //remove icon.zip
+                fs.unlink(k, function (err) {
+                    console.log(err);
+                });
+            });
+        });
+    }
+
+    function emitUninstall() {
+        eventEmitter.emit('endUninstall');
+    }
+
+    function restoreBak() {
+        fs.unlink(jsfile, function (err) {
+            var j = fs.createReadStream(jsfilebak).pipe(fs.createWriteStream(jsfile));
+            j.on('finish', function () {
+                fs.unlink(jsfilebak);
+            });
+        });
+        fs.unlink(cssfile, function (err) {
+            var c = fs.createReadStream(cssfilebak).pipe(fs.createWriteStream(cssfile));
+            c.on('finish', function () {
+                fs.unlink(cssfilebak);
+                vscode.window.showInformationMessage('Icons disabled. Restart the IDE.');
+                emitUninstall();
+            });
+        });
+    }
+
+
+    function fInstall() {
+        installItem(cssfilebak, cssfile, cleanCssInstall, replaceCss);
+        installItem(jsfilebak, jsfile, cleanJsInstall, replaceJs);
+        addIcons();
+    }
+
+    function fUninstall() {
+        fs.stat(jsfilebak, function (errBak, statsBak) {
+            if (errBak) {
+                vscode.window.showInformationMessage('Icons already disabled.');
+                emitUninstall();
+
+                return;
+            }
+            // checking if normal file has been udpated.
+            fs.stat(jsfile, function (errOr, statsOr) {
+                if (errOr) {
+                    vscode.window.showInformationMessage('Something went wrong: ' + errOr);
+                } else {
+                    var updated = hasBeenUpdated(statsBak, statsOr);
+                    if (updated) {
+                        // some update has occurred. clean install
+                        fs.unlink(jsfilebak);
+                        fs.unlink(cssfilebak);
+                        vscode.window.showInformationMessage('Icons disabled. Restart the IDE.');
+                        emitUninstall();
+                    } else {
+                        // restoring bak files
+                        restoreBak();
+                    }
+                }
+            });
+        });
+    }
+
+    function fReinstall() {
+        eventEmitter.once('endUninstall', fInstall);
+        fUninstall();
+    }
+
+    var installIcons = vscode.commands.registerCommand('extension.installIcons', fInstall);
+    var uninstallIcons = vscode.commands.registerCommand('extension.uninstallIcons', fUninstall);
+    var reinstallIcons = vscode.commands.registerCommand('extension.reinstallIcons', fReinstall);
+
+    context.subscriptions.push(installIcons);
+    context.subscriptions.push(uninstallIcons);
+    context.subscriptions.push(reinstallIcons);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
