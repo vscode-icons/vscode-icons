560ec132f685f1baa038dd910fa1d1f84626ee9c
Jens Hausdorf <jens1o@users.noreply.github.com>
2016-08-27 05:57:32 +0200

    include travis in supportedextensions

--- src/build/supportedExtensions.js
+++ src/build/supportedExtensions.js
@@ -122,6 +122,7 @@ exports.extensions = {
     { icon: 'text', extensions: ['txt'] },
     { icon: 'textile', extensions: ['textile'] },
     { icon: 'todo', extensions: ['todo'] },
+    { icon: 'travis', extensions: ['.travis.yml'] },
     { icon: 'twig', extensions: ['twig'] },
     { icon: 'typescript', extensions: ['ts'] },
     { icon: 'typescriptdef', extensions: ['.d.ts'], special: 'ts' },
