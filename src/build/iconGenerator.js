0a3ef2225378ee0755634dd170f08a3292c5628f
Roberto Huertas <roberto.huertas@outlook.com>
2016-09-10 02:13:50 +0200

    support for new contribution point

--- src/build/iconGenerator.js
+++ src/build/iconGenerator.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var _ = require('underscore');
 var files = require('./supportedExtensions').extensions;
 var folders = require('./supportedFolders').extensions;
+var ctype = require('./contribTypes');
 
 var folderDefs = _.uniq(_.map(folders.supported, function(o){
   return o.icon;
@@ -32,14 +33,29 @@ var fileDefs = _.uniq(_.map(files.supported, function(o){
   return old;
 }, {});
 
-// TODO FINISH THIS
-// var fileNames = _.reduce(folders.supported, function(old, current){
-//   current.extensions.forEach(function(o) {
-//     old.folders[o] = '_fd_' + current.icon;
-//     old.foldersOpen[o] = '_fd_' + current.icon + '_open';
-//   });
-//   return old;
-// }, {extensions: {}, names: {}, languages: {}});
+function removeFirstDot(txt){
+  if (txt.indexOf('.') === 0){
+    return txt.substring(1, txt.length);
+  } 
+  return txt;
+}
+
+var fileNames = _.reduce(files.supported, function(old, current){
+  var contribType = current.contribType;
+  var icon = current.icon;
+  current.extensions.forEach(function(o) {      
+    if (contribType === ctype.filename){
+      old.names[o] = '_f_' + icon;
+    } else if (!contribType){
+      old.extensions[removeFirstDot(o)] = '_f_' + icon;
+    } else {
+      old.languages[contribType] = '_f_' + icon;
+    }
+  });
+  return old;
+}, {extensions: {}, names: {}, languages: {}});
+
+console.dir(fileNames.extensions)
 
 function getDefaultSchema() {
   return {
@@ -80,9 +96,9 @@ function generate() {
   iconfile.iconDefinitions = Object.assign(iconfile.iconDefinitions, folderDefs, fileDefs);
   iconfile.folderNames = folderNames.folders;
   iconfile.folderNamesExpanded = folderNames.foldersOpen;
-  iconfile.fileNames = {}
-  iconfile.fileExtensions = {}
-  iconfile.languageIds = {}
+  iconfile.fileExtensions = fileNames.extensions;
+  iconfile.fileNames = fileNames.names;
+  iconfile.languageIds = fileNames.languages;
   buildJsonFile(iconfile);
 }
 
