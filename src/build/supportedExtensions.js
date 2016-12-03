28f7fa24d066f4792c2188453a0e805b5477165f
JimiC <jimikar@gmail.com>
2016-12-03 19:54:57 +0200

    Add support for Vim.

--- src/build/supportedExtensions.js
+++ src/build/supportedExtensions.js
@@ -189,8 +189,9 @@ exports.extensions = {
     { icon: 'vb', extensions: ['vb'] },
     { icon: 'vbhtml', extensions: ['vbhtml'] },
     { icon: 'vbproj', extensions: ['vbproj'] },
+    { icon: 'vim', extensions: ['vim'], svg: true },
+    { icon: 'vim', extensions: ['.vimrc', '.gvimrc'], contribType: ctype.filename, svg: true },
     { icon: 'volt', extensions: ['volt'] },
-    { icon: 'vue', extensions: ['vue'] },
     {
       icon: 'vscode', extensions: [
         'vscodeignore.json',
@@ -201,6 +202,7 @@ exports.extensions = {
         '.vscodeignore'
       ], contribType: ctype.filename
     },
+    { icon: 'vue', extensions: ['vue'] },
     {
       icon: 'webpack', extensions: [
         'webpack.config.js',
