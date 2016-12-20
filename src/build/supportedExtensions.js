8b6df870f0c55e79ce6b73d239a3159a6f26d061
Neil Kistner <neil.kistner@gmail.com>
2016-12-20 14:31:19 -0600

    Add Snyk icon

--- src/build/supportedExtensions.js
+++ src/build/supportedExtensions.js
@@ -184,6 +184,7 @@ exports.extensions = {
     { icon: 'slim', extensions: [] },
     { icon: 'sln', extensions: ['sln'] },
     { icon: 'smarty', extensions: ['tpl', 'swig'] },
+    { icon: 'snyk', extensions: ['.snyk'], filename: true, svg: true },
     { icon: 'source', extensions: [] },
     { icon: 'sql', extensions: ['sql'] },
     { icon: 'sqlite', extensions: ['sqlite', 'sqlite3', 'db3'] },
