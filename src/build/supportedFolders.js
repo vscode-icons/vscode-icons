f5afbcd4c4b8603b4d75a38f7cad6ce53aa49c32
Jimi (Dimitris) Charalampidis <jimikar@gmail.com>
2016-12-23 22:54:48 +0200

    Convert PNG to SVG (part 2). (#573)
    First batch for folders.

--- src/build/supportedFolders.js
+++ src/build/supportedFolders.js
@@ -1,26 +1,26 @@
 /* eslint-disable max-len */
 exports.extensions = {
   supported: [
-    { icon: 'bower', extensions: ['bower_components'] },
-    { icon: 'css', extensions: ['css'] },
-    { icon: 'dist', extensions: ['dist', 'out', 'export', 'build'] },
+    { icon: 'bower', extensions: ['bower_components'], svg: true },
+    { icon: 'css', extensions: ['css'], svg: true },
+    { icon: 'dist', extensions: ['dist', 'out', 'export', 'build'], svg: true },
     { icon: 'docs', extensions: ['docs'] },
     { icon: 'flow', extensions: ['flow'] },
-    { icon: 'git', extensions: ['github', 'git'], dot: true },
+    { icon: 'git', extensions: ['github', 'git'], dot: true, svg: true },
     { icon: 'haxelib', extensions: ['haxelib'] },
     { icon: 'images', extensions: ['images', 'img'] },
     { icon: 'less', extensions: ['less'] },
     { icon: 'locale', extensions: ['locale', 'locales'] },
-    { icon: 'node', extensions: ['node_modules'] },
+    { icon: 'node', extensions: ['node_modules'], svg: true },
     { icon: 'nuget', extensions: ['nuget'], dot: true },
     { icon: 'meteor', extensions: ['meteor'], dot: true },
-    { icon: 'sass', extensions: ['sass', 'scss'] },
+    { icon: 'sass', extensions: ['sass', 'scss'], svg: true },
     { icon: 'script', extensions: ['script', 'scripts'] },
     { icon: 'src', extensions: ['src', 'source', 'sources'] },
     { icon: 'style', extensions: ['style', 'styles'] },
     { icon: 'test', extensions: ['tests', 'test', '__tests__', '__test__', 'spec', 'specs'] },
     { icon: 'typings', extensions: ['typings'] },
-    { icon: 'vscode', extensions: ['vscode'], dot: true },
-    { icon: 'webpack', extensions: ['webpack'] }
+    { icon: 'vscode', extensions: ['vscode'], dot: true, svg: true },
+    { icon: 'webpack', extensions: ['webpack'], svg: true }
   ]
 };
