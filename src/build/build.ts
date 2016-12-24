27cca6b77c03d45a2664ba7e8bf694233830ec93
Roberto Huertas <roberto.huertas@outlook.com>
2016-12-24 03:11:05 +0100

    final touches

--- src/build/build.ts
+++ src/build/build.ts
@@ -1,20 +1,9 @@
 import { ncp } from 'ncp';
 import { generate } from './iconGenerator';
-const outDir = './dist';
+const outDir = './out/src/dev';
 
 // generating icons.json
 // The function takes as second argument the directory where
 // we want the file to be placed.
 // Default directory is the 'root' directory
 generate('icons.json', outDir);
-
-// moving to dist
-(<any> ncp).limit = 16;
-ncp('./src/dev', outDir, err => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  // tslint:disable no-console
-  console.log('Build completed!');
-});
