a87d9252d9ee4da2d522e6f6e7765ddcd8de65a1
Joshua Reilly <jreilly@lukava.co>
2017-02-17 11:17:47 -0700

    Keep both versions for backwards compatibility

--- src/icon-manifest/supportedExtensions.ts
+++ src/icon-manifest/supportedExtensions.ts
@@ -14,7 +14,7 @@ export const extensions: IFileCollection = {
     { icon: 'actionscript', extensions: [], languages: [languages.actionscript], format: FileFormat.svg },
     { icon: 'ai', extensions: ['ai'], format: FileFormat.svg },
     { icon: 'ai2', extensions: ['ai'], format: FileFormat.svg, disabled: true },
-    { icon: 'angular', extensions: ['.angular-cli.json'], filename: true, format: FileFormat.svg },
+    { icon: 'angular', extensions: ['.angular-cli.json', 'angular-cli.json'], filename: true, format: FileFormat.svg },
     { icon: 'ng_component_ts', extensions: ['component.ts'], format: FileFormat.svg },
     { icon: 'ng_component_js', extensions: ['component.js'], format: FileFormat.svg },
     { icon: 'ng_controller_ts', extensions: ['controller.ts'], format: FileFormat.svg },
