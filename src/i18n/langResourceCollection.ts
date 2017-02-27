1176afd7457d79ce31f7a203a2c26da722fe06c1
JimiC <jimikar@gmail.com>
2017-02-27 18:04:03 +0200

    Add French translation.

--- src/i18n/langResourceCollection.ts
+++ src/i18n/langResourceCollection.ts
@@ -1,8 +1,9 @@
 import { ILangResourceCollection } from '../models/i18n';
-import { langDe, langEn, langEs } from './langResources';
+import { langDe, langEn, langEs, langFr } from './langResources';
 
 export const langResourceCollection: ILangResourceCollection = {
   de: langDe,
   en: langEn,
   es: langEs,
+  fr: langFr,
 };
