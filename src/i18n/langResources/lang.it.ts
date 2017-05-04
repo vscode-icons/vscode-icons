efff8b61af8d74977b50bba79d8a480362d1e3d4
Sabin Rimniceanu <sabin.rimniceanu@gmail.com>
2017-05-04 15:24:09 +0200

    Add Italian Support
    Added Italian Language Support

---
+++ src/i18n/langResources/lang.it.ts
@@ -0,0 +1,40 @@
+import { ILangResource } from '../../models/i18n';
+
+export const langIt: ILangResource = {
+  newVersion: 'Benvenuto alla nuova versione di vscode-icons.',
+  seeReleaseNotes: 'Informazioni sui ultimi cambiamenti (in inglese)',
+  dontShowThis: 'Non mostrare più questo messaggio',
+  seeReadme: 'Sapere di più sull\'estensione',
+  welcomeBegin: 'vscode-icons ora usa la API ufficiale. Vai a',
+  welcomeEnd: 'e seleziona VSCode Icons per attivarla.',
+  activationPath: {
+    darwin: 'Code > Preferenze > Tema Icona File',
+    linux: 'File > Preferenze > Tema Icona File',
+    win32: 'File > Preferenze > Tema Icona File',
+  },
+  aboutOfficialApi: 'Sapere di più su icone per File & Cartelle (in inglese)',
+  learnMore: 'Vuoi saperne di più?',
+  reload: 'Riavvia',
+  autoReload: 'Riavvio Automatico',
+  disableDetect: 'Disabilita Rilevazione',
+  iconCustomization: 'Il manifesto delle icone sarà rigenerato.',
+  iconRestore: 'Il manifesto delle icone sarà restaurato.',
+  ngPresetEnabled: 'Le icone Angular saranno abilitate.',
+  ngPresetDisabled: 'Le icone Angular saranno disabilitate.',
+  jsOfficialPresetEnabled: 'Icone ufficiali JS saranno abilitate.',
+  jsOfficialPresetDisabled: 'Icone ufficiali JS saranno disabilitate.',
+  tsOfficialPresetEnabled: 'Icone ufficiali TS saranno abilitate.',
+  tsOfficialPresetDisabled: 'Icone ufficiali TS saranno disabilitate.',
+  jsonOfficialPresetEnabled: 'Icone ufficiali JSON saranno abilitate.',
+  jsonOfficialPresetDisabled: 'Icone ufficiali JSON saranno disabilitate.',
+  hideFoldersPresetEnabled: 'Visibilità icone cartelle sarà abilitata.',
+  hideFoldersPresetDisabled: 'Visibilità icone cartelle sarà disabilitata.',
+  foldersAllDefaultIconPresetEnabled: 'Icone specifiche per cartelle saranno abilitate.',
+  foldersAllDefaultIconPresetDisabled: 'Icone specifiche per cartelle saranno disabilitate.',
+  restart: 'Seleziona \'Riavvia\' affinché le modifiche abbiano effetto.',
+  ngDetected: 'vscode-icons ha rilevato un progetto Angular. ' +
+  'Seleziona \'Riavvia\' per abilitare le icone Angular.',
+  nonNgDetected: 'vscode-icons ha rilevato un progetto non Angular. ' +
+  'Seleziona \'Riavvia\' per disabilitare le icone Angular.',
+  projectDetecticonReset: 'Impostazioni Rilevazione Progetto Predefinite saranno ripristinate.',
+};
