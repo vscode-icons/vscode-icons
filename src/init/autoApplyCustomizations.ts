9cfe875a6bb90f7602a476a06cc2a0e166c35ed0
Jimi (Dimitris) Charalampidis <jimikar@gmail.com>
2017-05-29 16:47:25 +0300

    Fix condition check for user configuration settings. (#967)

--- src/init/autoApplyCustomizations.ts
+++ src/init/autoApplyCustomizations.ts
@@ -7,19 +7,20 @@ export function manageAutoApplyCustomizations(
   applyCustomizationCommand: () => void): void {
   if (!isNewVersion) { return; }
   const propObj = packageJson.contributes.configuration.properties as object;
-  for (const key in propObj) {
-    if (Reflect.has(propObj, key) && key !== 'vsicons.dontShowNewVersionMessage') {
-      const defaultValue = propObj[key].default;
-      const parts = key.split('.').filter(x => x !== 'vsicons');
-      const userValue = parts.reduce((prev, current) => prev[current], userConfig);
-      const cond1 = Array.isArray(defaultValue) && Array.isArray(userValue) && userValue.length;
-      // this is to equal null == undefined as vscode doesn't respect null defaults
-      // tslint:disable-next-line triple-equals
-      const cond2 = !Array.isArray(defaultValue) && defaultValue != userValue;
-      if (cond1 || cond2) {
-        applyCustomizationCommand();
-        return;
-      }
+  for (const key of Reflect.ownKeys(propObj) as string[]) {
+    if (!key.includes('presets') && !key.includes('associations')) {
+      continue;
+    }
+    const defaultValue = propObj[key].default;
+    const parts = key.split('.').filter(x => x !== 'vsicons');
+    const userValue = parts.reduce((prev, current) => prev[current], userConfig);
+    const cond1 = Array.isArray(defaultValue) && Array.isArray(userValue) && userValue.length;
+    // this is to equal null == undefined as vscode doesn't respect null defaults
+    // tslint:disable-next-line triple-equals
+    const cond2 = !Array.isArray(defaultValue) && defaultValue != userValue;
+    if (cond1 || cond2) {
+      applyCustomizationCommand();
+      return;
     }
   }
 }
