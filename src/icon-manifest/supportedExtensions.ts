b77130c19780310a142ef793b112d1243e99fc89
JimiC <jimikar@gmail.com>
2017-05-29 18:23:09 +0300

    Add support for .sequelizerc filename.

--- src/icon-manifest/supportedExtensions.ts
+++ src/icon-manifest/supportedExtensions.ts
@@ -311,6 +311,7 @@ export const extensions: IFileCollection = {
     { icon: 'scala', extensions: [], languages: [languages.scala], format: FileFormat.svg },
     { icon: 'script', extensions: [], languages: [languages.vbscript], format: FileFormat.svg },
     { icon: 'scss', extensions: ['scssm'], languages: [languages.scss], format: FileFormat.svg },
+    { icon: 'sequelize', extensions: ['.sequelizerc'], filename: true, format: FileFormat.svg },
     { icon: 'shaderlab', extensions: [], languages: [languages.shaderlab], light: true, format: FileFormat.svg },
     { icon: 'shell', extensions: ['fish'], languages: [languages.shellscript], format: FileFormat.svg },
     { icon: 'slim', extensions: [], languages: [languages.slim], format: FileFormat.svg },
