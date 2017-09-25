6e5e3a28c777c750cc828e28179bbbf2ab7cd04f
JimiC <jimikar@gmail.com>
2017-09-25 22:33:38 +0300

    Change locale structure to support translation service.

--- src/i18n/langResources/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './lang.de';
-export * from './lang.en';
-export * from './lang.es';
-export * from './lang.fr';
-export * from './lang.it';
-export * from './lang.ru';
-export * from './lang.zh-cn';
