8648a000fe4e8a9a1fd1a875704156a3b2564c75
Zachary Becknell <zbecknell@users.noreply.github.com>
2018-02-22 16:04:36 -0500

    Add support for T4 Text Templates

--- src/icon-manifest/supportedExtensions.ts
+++ src/icon-manifest/supportedExtensions.ts
@@ -523,6 +523,7 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     { icon: 'tslint', extensions: ['tslint.json', 'tslint.yaml', 'tslint.yml'], filename: true, format: FileFormat.svg },
+    { icon: 'tt', extensions: ['tt'], format: FileFormat.svg },
     { icon: 'twig', extensions: [], languages: [languages.twig], format: FileFormat.svg },
     { icon: 'typescript', extensions: [], languages: [languages.typescript], format: FileFormat.svg },
     { icon: 'typescript_official', extensions: [], languages: [languages.typescript], format: FileFormat.svg, disabled: true },
