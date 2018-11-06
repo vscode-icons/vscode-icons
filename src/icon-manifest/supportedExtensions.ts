309ae656979fbe6f2c7e9c6cc6dc94997f8bf1af
Manuel Serra <nitroin@hotmail.com>
2018-11-06 09:46:06 +0100

    Add support for Caddy (#1768)

--- src/icon-manifest/supportedExtensions.ts
+++ src/icon-manifest/supportedExtensions.ts
@@ -579,6 +579,13 @@ export const extensions: IFileCollection = {
       languages: [languages.cabal],
       format: FileFormat.svg,
     },
+    {
+      icon: 'caddy',
+      extensions: ['caddyfile'],
+      languages: [languages.caddyfile],
+      filename: true,
+      format: FileFormat.png
+    },
     { icon: 'cake', extensions: ['cake'], format: FileFormat.svg },
     { icon: 'cakephp', extensions: [], format: FileFormat.svg },
     {
