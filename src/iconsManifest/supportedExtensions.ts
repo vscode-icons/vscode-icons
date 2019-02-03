17ae4e074850b79ba086ec22caea23c11e809ddb
Roberto Huertas <roberto.huertas@outlook.com>
2019-02-03 13:45:09 +0100

    new golang logo and support for go.sum and go.mod

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -1255,6 +1255,12 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'go_package',
+      extensions: ['go.sum', 'go.mod'],
+      filename: true,
+      format: FileFormat.svg,
+    },
+    {
       icon: 'godot',
       extensions: [],
       languages: [languages.godot],
