3d1ca2a0903b55b2669c13d88ab1d024bf745249
KingDarBoja <mbojato@uninorte.edu.co>
2019-04-20 19:05:37 -0500

    Add support for Buildkite folder

--- src/iconsManifest/supportedFolders.ts
+++ src/iconsManifest/supportedFolders.ts
@@ -33,6 +33,7 @@ export const extensions: IFolderCollection = {
     { icon: 'aws', extensions: ['aws', '.aws'], format: FileFormat.svg },
     { icon: 'binary', extensions: ['bin', '.bin'], format: FileFormat.svg },
     { icon: 'bower', extensions: ['bower_components'], format: FileFormat.svg },
+    { icon: 'buildkite', extensions: ['.buildkite'], format: FileFormat.svg },
     { icon: 'cake', extensions: ['cake', '.cake'], format: FileFormat.svg },
     { icon: 'chef', extensions: ['chef', '.chef'], format: FileFormat.svg },
     { icon: 'circleci', extensions: ['.circleci'], format: FileFormat.svg },
