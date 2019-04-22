ec8fa9b89ef731f36a17bb0757e09451dad2436f
Roberto Huertas <roberto.huertas@outlook.com>
2019-04-22 11:55:37 +0200

    feat(coverage): support for coverage folder (#2015)

--- src/iconsManifest/supportedFolders.ts
+++ src/iconsManifest/supportedFolders.ts
@@ -105,6 +105,7 @@ export const extensions: IFolderCollection = {
       ],
       format: FileFormat.svg,
     },
+    { icon: 'coverage', extensions: ['coverage'], format: FileFormat.svg },
     { icon: 'css', extensions: ['css', '_css'], format: FileFormat.svg },
     { icon: 'cypress', extensions: ['cypress'], format: FileFormat.svg },
     {
