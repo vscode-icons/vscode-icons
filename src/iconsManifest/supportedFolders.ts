df2587e659a4122b86b8e6ca70d8ce2d91d479b5
KingDarBoja <mbojato@uninorte.edu.co>
2019-09-28 11:59:46 -0500

    Add support for Gradle folder

--- src/iconsManifest/supportedFolders.ts
+++ src/iconsManifest/supportedFolders.ts
@@ -183,6 +183,12 @@ export const extensions: IFolderCollection = {
     },
     { icon: 'github', extensions: ['.github'], format: FileFormat.svg },
     { icon: 'gitlab', extensions: ['.gitlab'], format: FileFormat.svg },
+    {
+      icon: 'gradle',
+      extensions: ['gradle', '.gradle'],
+      light: true,
+      format: FileFormat.svg,
+    },
     { icon: 'graphql', extensions: ['graphql'], format: FileFormat.svg },
     { icon: 'grunt', extensions: ['grunt'], format: FileFormat.svg },
     {
