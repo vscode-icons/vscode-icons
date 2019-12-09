40cfc8ece44fdac3892ba8315764b4860d3de6dd
Jimi (Dimitris) Charalampidis <jimikar@gmail.com>
2019-12-09 11:29:37 +0200

    Improving webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,36 +1,32 @@
-import webpack from 'webpack';
+import * as webpack from 'webpack';
 import { resolve } from 'path';
 import { constants } from './out/src/constants';
 
-const config: webpack.Configuration = {
-  target: 'node',
-  context: resolve(__dirname, 'out'),
-  output: {
-    path: resolve(__dirname, 'dist/src'),
-    filename: constants.extension.distEntryFilename,
-    libraryTarget: 'commonjs2',
-  },
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
-  externals: {
-    // The vscode-module is created on-the-fly and must be excluded.
-    // Add other modules that cannot be webpack'ed.
-    // 📖 -> https://webpack.js.org/configuration/externals/
-    vscode: 'commonjs vscode',
-  },
-}
+export default (_: any, argv: { mode: string }) => {
+  const config: webpack.Configuration = {
+    target: 'node',
+    context: resolve(__dirname, 'out'),
+    output: {
+      path: resolve(__dirname, 'dist/src'),
+      filename: constants.extension.distEntryFilename,
+      libraryTarget: 'commonjs2',
+    },
+    node: {
+      __dirname: false,
+      __filename: false,
+    },
+    externals: {
+      // The vscode-module is created on-the-fly and must be excluded.
+      // Add other modules that cannot be webpack'ed.
+      // 📖 -> https://webpack.js.org/configuration/externals/
+      vscode: 'commonjs vscode',
+    },
+  };
 
-/**
- * @param {any} env
- * @param {{mode: string}} argv
- */
-export = (_env: any, argv: { mode: string }) => {
   // development mode only
   if (argv && argv.mode === 'development') {
     config.devtool = 'source-map';
-    config.output.devtoolModuleFilenameTemplate = '../../[resource-path]'
+    config.output.devtoolModuleFilenameTemplate = '../../[resource-path]';
     return config;
   }
   return config;
