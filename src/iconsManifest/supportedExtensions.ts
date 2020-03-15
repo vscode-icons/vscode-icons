8cac144eef0c44cdb8db81e76b7c2c3b93b2f487
KingDarBoja <stevenbojato04@gmail.com>
2020-03-15 00:29:04 -0500

    Add support for GraphQL config

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -1569,6 +1569,14 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'graphql_config',
+      extensions: ['.graphqlconfig'],
+      filenamesGlob: ['.graphqlconfig'],
+      extensionsGlob: ['yml', 'yaml'],
+      filename: true,
+      format: FileFormat.svg,
+    },
+    {
       icon: 'graphviz',
       extensions: [],
       languages: [languages.graphviz],
