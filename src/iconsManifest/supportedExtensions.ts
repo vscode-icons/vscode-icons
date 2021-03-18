e185826d9ac70d0185c74a28e03b6461011e59d7
Mike Valeriano <mkvlrn@gmail.com>
2021-03-18 13:23:59 -0300

    Add support for file 'tailwind.config.json' in the 'tailwind' icon

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -3782,7 +3782,7 @@ export const extensions: IFileCollection = {
       icon: 'tailwind',
       extensions: [],
       filenamesGlob: ['tailwind', 'tailwind.config'],
-      extensionsGlob: ['js', 'coffee', 'ts'],
+      extensionsGlob: ['js', 'coffee', 'ts', 'json'],
       filename: true,
       format: FileFormat.svg,
     },
