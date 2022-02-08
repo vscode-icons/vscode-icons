faa797355d12c24763aa528ce7b6fe394c03ed1d
Mike Valeriano <mkvlrn@gmail.com>
2022-02-07 21:40:49 -0300

    use single quotes

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -3924,7 +3924,7 @@ export const extensions: IFileCollection = {
     {
       icon: 'tailwind',
       extensions: [],
-      filenamesGlob: ['tailwind', 'tailwind.config', ".tailwind", ".tailwindrc"],
+      filenamesGlob: ['tailwind', 'tailwind.config', '.tailwind', '.tailwindrc'],
       extensionsGlob: ['js', 'cjs', 'coffee', 'ts', 'json'],
       filename: true,
       format: FileFormat.svg,
