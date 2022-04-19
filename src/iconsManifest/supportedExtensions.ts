3288a73bb760b439ac9d68518d6b5cc558379b1b
Sasial <44125644+sasial-dev@users.noreply.github.com>
2022-04-19 11:15:03 +1000

    refactor(gnu): rename gnu logo and add .ld files

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -1742,6 +1742,12 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'gnu',
+      extensions: ['ld'],
+      languages: [languages.makefile],
+      format: FileFormat.svg,
+    },
+    {
       icon: 'gnuplot',
       extensions: [],
       languages: [languages.gnuplot],
@@ -2447,12 +2453,6 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     { icon: 'lync', extensions: ['crec', 'ocrec'], format: FileFormat.svg },
-    {
-      icon: 'makefile',
-      extensions: ['makefile'],
-      languages: [languages.makefile],
-      format: FileFormat.svg,
-    },
     {
       icon: 'manifest',
       extensions: ['manifest'],
