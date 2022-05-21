6eb0302dd1d5163e02a7cb8ff3fc1f1669016cd2
Sasial <44125644+sasial-dev@users.noreply.github.com>
2022-05-22 00:42:18 +1000

    feat(git-icon): add .issuetracker (#2988)
    Co-authored-by: Roberto Huertas <roberto.huertas@outlook.com>

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -1690,6 +1690,7 @@ export const extensions: IFileCollection = {
         '.gitmodules',
         '.gitkeep',
         '.mailmap',
+        '.issuetracker',
       ],
       filename: true,
       languages: [languages.git],
