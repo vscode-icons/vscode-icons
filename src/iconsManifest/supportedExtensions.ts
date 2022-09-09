2647389deedaf172e39b71fc83d3a2fe30628fe9
Sasial <44125644+sasial-dev@users.noreply.github.com>
2022-09-10 08:08:56 +1000

    feat(icon): add wally icon

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -4728,6 +4728,12 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'wally',
+      extensions: ['wally.toml', 'wally.lock'],
+      filename: true,
+      format: FileFormat.svg,
+    },
+    {
       icon: 'watchmanconfig',
       extensions: ['.watchmanconfig'],
       filename: true,
