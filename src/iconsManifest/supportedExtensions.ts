781c20c1384b223b1ff900bedf4e29fcea66b599
Jan Vollmer <jan@vllmr.dev>
2023-12-21 22:15:11 +0100

    add icons for pdm files

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -3343,6 +3343,14 @@ export const extensions: IFileCollection = {
       disabled: true,
     },
     {
+      icon: 'pdm',
+      extensions: [],
+      filenamesGlob: ['pdm'],
+      extensionsGlob: ['lock', 'toml'],
+      filename: true,
+      format: FileFormat.svg,
+    },
+    {
       // Taken from https://peeky.dev/logo.svg
       icon: 'peeky',
       // See: https://peeky.dev/guide/config.html
