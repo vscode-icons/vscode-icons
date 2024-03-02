0e6ef76dfd5bcaf988d77d1b32dd57f480ec679c
Ar-mane <abderrahmane.arache@gmail.com>
2024-03-02 17:42:52 +0100

    feat: add cmrc icon support

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -2828,6 +2828,11 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'cmrc',
+      extensions: ['cmrc'],
+      format: FileFormat.svg,
+    },
+    {
       icon: 'julia',
       extensions: [],
       languages: [languages.julia],
