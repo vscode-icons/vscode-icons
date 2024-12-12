1f6df95f50af8400efcf51addf56b7df89653b80
Remco Haszing <remcohaszing@gmail.com>
2024-12-12 15:37:58 +0100

    feat(icon): support the cypher language
    There are multiple extensions that register the `cypher` language,
    including the official one on
    https://marketplace.visualstudio.com/items?itemName=neo4j-extensions.neo4j-for-vscode
    
    Closes #3630

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -3209,6 +3209,13 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'neo4j',
+      extensions: [],
+      languages: [languages.cypher],
+      light: true,
+      format: FileFormat.svg,
+    },
+    {
       icon: 'nestjs',
       extensions: [
         '.nest-cli.json',
