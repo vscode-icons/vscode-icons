b6131cc2d45505c437d5761edd6eb76a1e6a0cd9
Remco Haszing <remcohaszing@gmail.com>
2024-12-12 15:48:23 +0100

    feat(icon): add the Minecraft file icon
    Closes #2479

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -3174,6 +3174,12 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'minecraft',
+      extensions: ['.mca', '.mcmeta'],
+      languages: [languages.minecraft],
+      format: FileFormat.svg,
+    },
+    {
       icon: 'mivascript',
       extensions: [],
       languages: [languages.mv],
