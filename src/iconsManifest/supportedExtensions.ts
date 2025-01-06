b9e26b1b5fb9335cf8a99be349685c76f4dc848a
Remco Haszing <remcohaszing@gmail.com>
2025-01-06 12:20:33 +0100

    feat(icon): support eleventy.config.mjs
    Support for `eleventy.config.mjs` was added in Eleventy 3.0.0.
    
    See https://www.11ty.dev/docs/config/#default-filenames

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -1676,14 +1676,24 @@ export const extensions: IFileCollection = {
     { icon: 'elasticbeanstalk', extensions: [], format: FileFormat.svg },
     {
       icon: 'eleventy',
-      extensions: ['.eleventy.js', 'eleventy.config.js', 'eleventy.config.cjs'],
+      extensions: [
+        '.eleventy.js',
+        'eleventy.config.js',
+        'eleventy.config.cjs',
+        'eleventy.config.mjs',
+      ],
       filename: true,
       light: true,
       format: FileFormat.svg,
     },
     {
       icon: 'eleventy2',
-      extensions: ['.eleventy.js', 'eleventy.config.js', 'eleventy.config.cjs'],
+      extensions: [
+        '.eleventy.js',
+        'eleventy.config.js',
+        'eleventy.config.cjs',
+        'eleventy.config.mjs',
+      ],
       filename: true,
       light: true,
       format: FileFormat.svg,
