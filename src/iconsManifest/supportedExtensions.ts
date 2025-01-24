8b6686ae4e7af69f568e2c350ddad6b4633abf71
Remco Haszing <remcohaszing@gmail.com>
2025-01-24 15:38:49 +0100

    feat(icon): add lark icon
    The `lark` language ID is registered by
    https://marketplace.visualstudio.com/items?itemName=dirk-thomas.vscode-lark.
    
    The icon was taken from
    https://github.com/lark-parser/ide/blob/master/static/favicon.svg.
    
    Closes #3559

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -2849,6 +2849,13 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'lark',
+      extensions: [],
+      languages: [languages.lark],
+      light: true,
+      format: FileFormat.svg,
+    },
+    {
       icon: 'layout',
       extensions: ['layout.html', 'layout.htm'],
       filename: true,
