9aad9b2c4a7f12409ab56b53ad47fda3050ad5e5
ZhengYu, Xu <zen-xu@outlook.com>
2025-02-06 13:11:52 +0800

    feat(icon): add pixi icon

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -6162,5 +6162,14 @@ export const extensions: IFileCollection = {
       filename: true,
       format: FileFormat.svg,
     },
+    {
+      icon: 'pixi',
+      extensions: [
+        'pixi.lock',
+        'pixi.toml',
+      ],
+      filename: true,
+      format: FileFormat.svg,
+    },
   ],
 };
