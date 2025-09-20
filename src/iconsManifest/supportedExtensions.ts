3579c513f1af1ec212309bc64cd9f21b851d7923
tijn <tijn@tijn.dev>
2025-09-20 20:49:57 +0200

    feat: support ripple

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -4740,6 +4740,12 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'ripple',
+      extensions: ['ripple'],
+      languages: [languages.ripple],
+      format: FileFormat.svg,
+    },
+    {
       icon: 'rnc',
       extensions: ['rnc'],
       languages: [languages.rnc],
