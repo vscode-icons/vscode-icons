2db844cfea3c12f4d3ec2e91964ad6c352ee8e1e
Matheus Mortari <matheus.felipe.19rt@gmail.com>
2025-11-24 20:01:31 -0300

    feat(icon): add prisma.config icon

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -4455,6 +4455,14 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'prismaconfig',
+      extensions: [],
+      filenamesGlob: ['prisma.config'],
+      extensionsGlob: ['js', 'ts'],
+      filename: true,
+      format: FileFormat.svg,
+    },
+    {
       icon: 'processinglang',
       extensions: [],
       languages: [languages.processinglang],
