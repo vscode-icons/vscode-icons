4fd190ffca5bdadaf306720672cb652131597730
Akihiro Nagai <77012577+314systems@users.noreply.github.com>
2026-05-05 19:37:22 +0900

    feat: add textlint icon and supported file extensions (#4044)

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -5822,6 +5822,20 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'textlint',
+      extensions: [
+        '.textlintrc',
+        '.textlintrc.js',
+        '.textlintrc.cjs',
+        '.textlintrc.json',
+        '.textlintrc.yml',
+        '.textlintrc.yaml',
+        '.textlintignore',
+      ],
+      filename: true,
+      format: FileFormat.svg,
+    },
+    {
       icon: 'tiltfile',
       extensions: ['.tiltignore'],
       filename: true,
