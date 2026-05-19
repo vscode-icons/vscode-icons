4cb5fbf2725a9df2322b1dddbd7aca1083cfef34
Roberto Huertas <roberto.huertas@outlook.com>
2026-05-19 17:22:51 +0200

    fix(extension): refresh package-lock.json tree (#4054)

--- src/models/extensions/folderCollection.ts
+++ src/models/extensions/folderCollection.ts
@@ -2,7 +2,6 @@ import { IExtensionCollection } from './extensionCollection';
 import { IFolderExtension } from './folderExtension';
 import { IFolderDefault } from './folderDefault';
 
-export interface IFolderCollection
-  extends IExtensionCollection<IFolderExtension> {
+export interface IFolderCollection extends IExtensionCollection<IFolderExtension> {
   default: IFolderDefault;
 }
