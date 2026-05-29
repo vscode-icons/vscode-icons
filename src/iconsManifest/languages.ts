65a77d1d67275a96402fd341c0f909a664f97e53
Muhammad Naufal Kateni <75259474+NaufalK25@users.noreply.github.com>
2026-05-29 21:01:42 +0700

    feat: remove .psql and .sql file extension from postgres
    Co-authored-by: Remco Haszing <remcohaszing@gmail.com>

--- src/iconsManifest/languages.ts
+++ src/iconsManifest/languages.ts
@@ -445,7 +445,7 @@ export const languages = {
   polymer: { ids: 'polymer', knownExtensions: ['polymer'] },
   pony: { ids: 'pony', knownExtensions: ['pony'] },
   postcss: { ids: 'postcss', knownExtensions: ['pcss'] },
-  postgres: { ids: 'postgres', knownExtensions: ['pgsql', 'psql', 'sql'] },
+  postgres: { ids: 'postgres', knownExtensions: ['pgsql'] },
   postscript: { ids: 'postscript', knownExtensions: ['ps'] },
   powershell: { ids: 'powershell', knownExtensions: ['ps1'] },
   prisma: { ids: 'prisma', knownExtensions: ['prisma'] },
