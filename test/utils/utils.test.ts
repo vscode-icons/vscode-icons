8a7bba70771b47575f0556cce037969063f297a9
Remco Haszing <remcohaszing@gmail.com>
2026-06-11 20:10:45 +0200

    chore: replace remaining proxyquire import

--- test/utils/utils.test.ts
+++ test/utils/utils.test.ts
@@ -3,7 +3,7 @@ import { ChildProcess } from 'child_process';
 import { Stats } from 'fs';
 import * as os from 'os';
 import * as path from 'path';
-import * as proxyq from 'proxyquire';
+import proxyq from 'proxyquire';
 import * as sinon from 'sinon';
 import * as fsAsync from '../../src/common/fsAsync';
 import { FileFormat } from '../../src/models';
