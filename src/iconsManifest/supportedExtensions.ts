1b15f92ef69fb719981a8b920674a8bfb6343d71
Muhammad Naufal Kateni <75259474+NaufalK25@users.noreply.github.com>
2026-06-11 18:26:43 +0700

    fix(icon): remove duplicate postgresql icon (#4093)

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -4587,12 +4587,6 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
-      icon: 'postgres',
-      extensions: [],
-      languages: [languages.postgres],
-      format: FileFormat.svg,
-    },
-    {
       icon: 'postman',
       extensions: [
         'postman_collection.json',
