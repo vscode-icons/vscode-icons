d8abe6a1318f92733bd60c8c3e3727e2daf754b8
Muhammad Naufal Kateni <naufalkateni2001@gmail.com>
2026-06-30 08:42:52 +0700

    feat(icon): add rector icon

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -5163,6 +5163,12 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'rector',
+      extensions: ['rector.php'],
+      filename: true,
+      format: FileFormat.svg,
+    },
+    {
       icon: 'red',
       extensions: [],
       languages: [languages.red],
