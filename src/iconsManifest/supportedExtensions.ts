73339e91ee9ae8a47f693d75f19da5ba979de6ae
Michael Herwig <develop+github@michael-herwig.de>
2026-07-06 10:05:36 +0200

    feat(icon): add the ocx icon (#4152)

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -4263,6 +4263,12 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'ocx',
+      extensions: ['ocx.toml', 'ocx.lock'],
+      filename: true,
+      format: FileFormat.svg,
+    },
+    {
       icon: 'dune',
       extensions: ['dune', 'dune-project', 'dune-workspace'],
       filenamesGlob: [],
