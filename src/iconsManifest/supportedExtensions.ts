7401eac6ae7b1f826b8807706ba83efcf969a9a2
Muhammad Naufal Kateni <75259474+NaufalK25@users.noreply.github.com>
2026-08-14 16:05:10 +0700

    feat(icon): extend buckbuild filename entry (#4194)
    Add support for more buck.build filename.
    - [`.buckconfig.local`](https://buck.build/files-and-dirs/buckconfig.html) - local version of `.buckconfig` file
    - [`.buckjavaargs`](https://buck.build/files-and-dirs/buckjavaargs.html) - config file to read JVM flags
    - [`BUCK`](https://buck.build/concept/build_file.html) - default build file

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -936,7 +936,7 @@ export const extensions: IFileCollection = {
     },
     {
       icon: 'buckbuild',
-      extensions: ['.buckconfig'],
+      extensions: ['.buckconfig', '.buckconfig.local', '.buckjavaargs', 'BUCK'],
       filename: true,
       format: FileFormat.svg,
     },
