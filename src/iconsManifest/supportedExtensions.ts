73172b8cb2400c386fd6fd27834d45125a013d9f
Muhammad Naufal Kateni <75259474+NaufalK25@users.noreply.github.com>
2026-08-14 15:34:33 +0700

    feat(icon): extend dvc entry filename (#4191)
    Here is the list of added files:
    - [`.dvcignore`](https://doc.dvc.org/user-guide/project-structure/dvcignore-files#dvcignore-files)
    - [`dvc.yaml`](https://doc.dvc.org/user-guide/project-structure/dvcyaml-files#dvcyaml)
    - [`dvc.lock`](https://doc.dvc.org/user-guide/project-structure/dvcyaml-files#dvclock-file)
    
    Also updated the [dvc icon](https://github.com/treeverse/dvc.org/blob/main/static/logos/dvc.svg), since it's a little different visually from the current one, especially the `V` part.

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -1963,7 +1963,7 @@ export const extensions: IFileCollection = {
     },
     {
       icon: 'dvc',
-      extensions: ['.dvc'],
+      extensions: ['.dvc', '.dvcignore', 'dvc.yaml', 'dvc.lock'],
       languages: [],
       filename: true,
       format: FileFormat.svg,
