6b44f3b62d1f5748efd2788b646c40d7356542ce
Muhammad Naufal Kateni <naufalkateni2001@gmail.com>
2026-09-14 10:34:27 +0700

    feat(icon): add HIP icon
    Add support for [HIP](https://rocm.docs.amd.com/projects/HIP/en/latest/). `.hip` files are already recognized under the `cpp` language ID by https://marketplace.visualstudio.com/items?itemName=ms-vscode.cpptools, but similar to how `.cu` files use the NVIDIA icon, I think we should associate `.hip` with the AMD icon.
    
    Icon was taken from https://github.com/ROCm/rocm-docs-core/tree/develop/src/rocm_docs/rocm_docs_theme/static/images.

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -2835,6 +2835,12 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'hip',
+      extensions: ['hip'],
+      light: true,
+      format: FileFormat.svg,
+    },
+    {
       icon: 'histoire',
       extensions: [
         'histoire.config.ts',
