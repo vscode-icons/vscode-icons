1d7951d9358c268c94fca06ad1e442fb4357bde6
Nut2026 <256511636+Nut2026@users.noreply.github.com>
2026-09-27 04:22:28 +0800

    feat(icon): add dedicated csv icon (#4125)

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -1554,6 +1554,12 @@ export const extensions: IFileCollection = {
       disabled: true,
     },
     {
+      icon: 'csv',
+      extensions: ['csv', 'tsv'],
+      languages: [languages.csv],
+      format: FileFormat.svg,
+    },
+    {
       icon: 'cucumber',
       extensions: [],
       languages: [languages.cucumber],
@@ -6345,8 +6351,8 @@ export const extensions: IFileCollection = {
     },
     {
       icon: 'text',
-      extensions: ['csv', 'tsv'],
-      languages: [languages.csv, languages.plaintext],
+      extensions: ['txt'],
+      languages: [languages.plaintext],
       format: FileFormat.svg,
     },
     {
